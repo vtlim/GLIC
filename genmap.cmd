50c846c0ea2b43053c055164349305ffb32977a0
Victoria Lim <lim.t.victoria@gmail.com>
2019-07-11 16:19:02 +0200

    fix chimera genmap issues

--- genmap.cmd
+++ genmap.cmd
@@ -4,9 +4,14 @@
 open straight_molmap_15a.mrc 
 
 # generate map of structure #0 loaded from python 
-molmap #0 15 model #2 onGrid #1
+# grid spacing will be set to 1/3 of specified resolution
+
+# [not working] molmap #0 2.49 model #2 onGrid #1
+
+molmap #0 2.49 model #2
+vop resample #2 onGrid #1
 
 # save the new map
-volume #2 save temp.mrc
+volume #3 save temp.mrc
 
 stop
