8d5d31693e7e3e60582ae5754d113ae11f505822
Victoria Lim <lim.t.victoria@gmail.com>
2019-07-18 16:48:31 +0200

    fix ordering for inverting structure to map

--- tempy_pdb.py
+++ tempy_pdb.py
@@ -17,17 +17,17 @@ target_map=MapParser.readMRC(sys.argv[1]) #read target map
 # read in structure
 structure_instance=PDBParser.read_PDB_file('structure_id', sys.argv[2])
 
+# translate along x, y, z
+structure_instance.translate(42, 58, -5)
+
+# rotate along x, y, z
+structure_instance.rotate_by_axis_angle(0,0,1, np.rad2deg(-2.125868534775962), com=com)
+structure_instance.rotate_by_axis_angle(0,1,0, np.rad2deg(-0.0005038746980934731), com=com)
+structure_instance.rotate_by_axis_angle(1,0,0, np.rad2deg(3.1396619777494124), com=com)
+
 # create the map
 blurrer = StructureBlurrer()
 sim_map = blurrer.gaussian_blur(structure_instance, 2.49, densMap=target_map)
-
-# translate along x, y, z
-sim_map = sim_map.translate(42, 58, -5)
-
-# rotate along x, y, z
-sim_map = sim_map.rotate_by_axis_angle(1,0,0, np.rad2deg(3.1396619777494124), com)
-sim_map = sim_map.rotate_by_axis_angle(0,1,0, np.rad2deg(-0.0005038746980934731), com)
-sim_map = sim_map.rotate_by_axis_angle(0,0,1, np.rad2deg(-2.125868534775962), com)
 
 # save map
 sim_map.write_to_MRC_file('moved.mrc') # Writing out to MRC file
