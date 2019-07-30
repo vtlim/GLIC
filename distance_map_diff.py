ffcbd8a87a13d6b52312ded2cc80d7c8554fb6ce
Victoria Lim <lim.t.victoria@gmail.com>
2019-07-30 16:24:28 +0200

    modularize; argparse; process single input map

--- distance_map_diff.py
+++ distance_map_diff.py
@@ -11,18 +11,54 @@ def extract_txt_file(txtfile):
     array = np.genfromtxt(txtfile)
     return array
 
-# extract data from txt file
-array1 = extract_txt_file(sys.argv[1])
-array2 = extract_txt_file(sys.argv[2])
-diff = array2 - array1
+def get_range(array):
 
-# define heat map limits symmetrically so that value of zero is white
-maxval = np.amax(diff)
-minval = np.amin(diff)
-absval = max(abs(maxval), abs(minval))
+    # ignore nan in min or max
+    maxval = np.nanmax(array)
+    minval = np.nanmin(array)
+    absval = max(abs(maxval), abs(minval))
 
-# plot data
-plt.imshow(diff, cmap='seismic', origin='lower', vmin=-absval, vmax=absval)
-plt.colorbar()
-plt.savefig('diff_DistanceMap.eps')
-plt.show()
+    return absval
+
+def map_diff(file1, file2):
+    # extract data from txt file
+    array1 = extract_txt_file(file1)
+    array2 = extract_txt_file(file2)
+    diff = array2 - array1
+
+    # define heat map limits symmetrically so that value of zero is white
+    absval = get_range(diff)
+
+    # plot data
+    plt.imshow(diff, cmap='seismic', origin='lower', vmin=-absval, vmax=absval)
+    plt.colorbar()
+    plt.savefig('diff_DistanceMap.eps')
+    plt.show()
+
+def map_single(file1):
+
+    # extract data from txt file
+    array1 = extract_txt_file(file1)
+
+    # plot data
+    plt.imshow(array1, cmap='Oranges', origin='lower')
+    plt.colorbar()
+    plt.savefig('heatmap.eps')
+    plt.show()
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--infile", nargs='+',
+                        help="Name of the input file(s).")
+    parser.add_argument("-s", "--single", action='store_true',
+                        help="Only plot a single map and not difference map")
+
+    args = parser.parse_args()
+
+    if args.single:
+        map_single(args.infile[0])
+    else:
+        map_diff(args.infile[0], args.infile[1])
