6dab4e5abae69624f2ce621b34021b1e35483e70
Victoria Lim <lim.t.victoria@gmail.com>
2019-07-30 16:23:42 +0200

    rmsd by each residue not all by all

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Last updated: Jul 30 2019
 | `genmask.cmd`         | chimera           | generate mask for map from around a reference protein structure       |
 | `natsort.py`          | python            | import from other scripts for natural sort of file names              |
 | `record_plane.cmd`    | chimera           | open in chimera to generate movie of plane snapshot over varying z    |
-| `rmsd_pairwise.tcl`   | vmd               | compute all by all pairwise rmsd of protein residues
+| `rmsd_residue.tcl`    | vmd               | compute RMSD for each residue backbone between two structures         |
 | `tempy.py`            | python            | example application of moving density with input rotation/translation | 
 | `tempy_pdb.py`        | python            | related to `tempy.py`; now take *structure* and move to orig map      | 
 | `transformations.py`  | python            | called by `decompose_matrix.py` to decompose 4x4 move matrix          | 
@@ -33,4 +33,5 @@ For files that are not actively used in the project but may come into handy.
 |-----------------------|-------------------|-----------------------------------------------------------------------|
 | `contact_map.py`      | python            | generate contact map (not distance map) on two PDBs to compare        |
 | `density_gromaps.sh`  | bash, gromaps     | create run file for gromaps to write densities from gromacs trajectory|
+| `rmsd_pairwise.tcl`   | vmd               | compute all by all pairwise rmsd of protein residues
 
---
+++ rmsd_residue.tcl
@@ -0,0 +1,75 @@
+
+# __________________________________________________________________________________
+#
+# rmsd_pairwise.tcl
+# Purpose:          Compute RMSD between each pair of residues in two GLIC structures.
+# Usage:            vmdt -e rmsd_pairwise.tcl -args file1.pdb file2.pdb
+#
+# TODO:
+# - Redundant code from align_tmd.tcl
+#
+# Notes:
+# - VMD can take in .gro files in place of .pdb files.
+# __________________________________________________________________________________
+
+
+# maybe skip terminal resid 315 if vmd is unable to read in gro
+set tmd_sel "alpha and (resid 195 to 314) and (not resid 242 to 253)"
+
+set ref_pdb [lindex $argv 0]
+set mov_pdb [lindex $argv 1]
+
+# =============================================================== #
+# load in structures and align
+
+mol new $ref_pdb
+mol new $mov_pdb
+
+set ref_sel [atomselect 0 "$tmd_sel"]
+set mov_sel [atomselect 1 "$tmd_sel"]
+set mov_all [atomselect 1 "all and not water"]
+#set mov_all [atomselect 1 "protein"]
+
+set matrix [measure fit $mov_sel $ref_sel]
+
+$mov_all move $matrix
+
+# =============================================================== #
+
+# check that number of residues match in both structures
+set nres0 [llength [lsort -unique -integer [[atomselect 0 all] get residue]]]
+set nres1 [llength [lsort -unique -integer [[atomselect 1 all] get residue]]]
+
+# without expr, VMD doesn't evaluate bc: invalid command name "1555"
+if {! [expr $nres0 == $nres1] } {
+    puts "\n\nERROR: number of residues do not match. $nres0 $nres1\n"
+    exit
+}
+
+# loop over each residue to do an NxN comparison of RMSD
+puts "\nCalculating pairwise RMSD by each residue's backbone..."
+set outDataFile [open rmsd_matrix.txt w]
+for {set i 0} {$i < $nres0} {incr i} {
+
+    set curr_line "$i"
+    set sel0 [atomselect 0 "backbone and residue $i"]
+    set sel1 [atomselect 1 "backbone and residue $i"]
+
+    # happens for the terminal residue in each subunit
+    if { [expr [$sel0 num] == 0] || [expr [$sel1 num] == 0] } {
+        puts "\n\nWARNING: zero atoms read for one of residue $i"
+        set curr_line "$curr_line\tNaN"
+        continue
+    }
+
+    set rmsdsel [format "%.4f" [measure rmsd $sel1 $sel0]]
+    set curr_line "$curr_line\t$rmsdsel"
+
+    # free memory to prevent VMD from getting killed
+    $sel1 delete
+    $sel0 delete
+
+    puts $outDataFile $curr_line
+}
+
+close $outDataFile
