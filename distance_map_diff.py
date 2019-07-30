f21dce2e178a99b09ad2e135a629be09c4ed7215
Victoria Lim <lim.t.victoria@gmail.com>
2019-07-30 10:17:02 +0200

    symmetrize scale

--- distance_map_diff.py
+++ distance_map_diff.py
@@ -11,11 +11,18 @@ def extract_txt_file(txtfile):
     array = np.genfromtxt(txtfile)
     return array
 
+# extract data from txt file
 array1 = extract_txt_file(sys.argv[1])
 array2 = extract_txt_file(sys.argv[2])
 diff = array2 - array1
 
-plt.imshow(diff, cmap='seismic', origin='lower')
+# define heat map limits symmetrically so that value of zero is white
+maxval = np.amax(diff)
+minval = np.amin(diff)
+absval = max(abs(maxval), abs(minval))
+
+# plot data
+plt.imshow(diff, cmap='seismic', origin='lower', vmin=-absval, vmax=absval)
 plt.colorbar()
 plt.savefig('diff_DistanceMap.eps')
 plt.show()
