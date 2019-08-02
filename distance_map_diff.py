3884440deb07474f15ae9642c008234fe8b6eab7
Victoria Lim <lim.t.victoria@gmail.com>
2019-08-02 13:13:38 -0700

    extend argparse options; change parsing function name

--- distance_map_diff.py
+++ distance_map_diff.py
@@ -4,7 +4,7 @@ import sys
 import numpy as np
 import matplotlib.pylab as plt
 
-def extract_txt_file(txtfile):
+def parse_txt_file(txtfile):
     """
     Extract distances from text file generated with nanoHUB contact maps tool.
     """
@@ -21,9 +21,9 @@ def get_range(array):
     return absval
 
 def map_diff(file1, file2):
-    # extract data from txt file
-    array1 = extract_txt_file(file1)
-    array2 = extract_txt_file(file2)
+    # parse data from txt file
+    array1 = parse_txt_file(file1)
+    array2 = parse_txt_file(file2)
     diff = array2 - array1
 
     # define heat map limits symmetrically so that value of zero is white
@@ -37,8 +37,8 @@ def map_diff(file1, file2):
 
 def map_single(file1):
 
-    # extract data from txt file
-    array1 = extract_txt_file(file1)
+    # parse data from txt file
+    array1 = parse_txt_file(file1)
 
     # plot data
     plt.imshow(array1, cmap='Oranges', origin='lower')
@@ -51,10 +51,30 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
 
+    # basic options
     parser.add_argument("-i", "--infile", nargs='+',
                         help="Name of the input file(s).")
     parser.add_argument("-s", "--single", action='store_true',
                         help="Only plot a single map and not difference map")
+
+    # options for extracting subplots
+    parser.add_argument("-u", "--residue", nargs='+',
+                        help="Analgous to VMD 'residue' selection. Residue "
+                            "number(s) should match file's line number(s)."
+                            "This option extracts the specified lines for"
+                            "plotting.")
+    parser.add_argument("-d", "--resid", nargs='+',
+                        help="Analgous to VMD 'resid' selection. If using "
+                            "this option, you must also specify number of "
+                            "residues in a single subunit with -n flag AND "
+                            "the number of subunits with -- flag."
+                            "For each input i, the ith residue of each "
+                            "subunit is extracted for plotting.")
+    parser.add_argument("-p", "--parts", type=int,
+                        help="How many subunits (analogous parts) comprise "
+                             "the protein?")
+    parser.add_argument("-e", "--each", type=int,
+                        help="How many residues are in each identical subunit?")
 
     args = parser.parse_args()
 
